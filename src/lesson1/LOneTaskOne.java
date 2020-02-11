1bcfee33e7c4c4633de93a1bbff84964957f6b59
Lenovo-PC <e999or@bk.ru>
2020-02-12 00:20:15 +0300

    Initial commit

---
+++ src/lesson1/LOneTaskOne.java
@@ -0,0 +1,22 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class LOneTaskOne {
+    public static void main(String[] args){
+        System.out.println("Введите первое число");
+        int firstNum = (new Scanner(System.in)).nextInt();
+        System.out.println("Введите второе число");
+        int secondNum = (new Scanner(System.in)).nextInt();
+
+        if(firstNum < secondNum){
+            System.out.println("Первое число минимальное");
+        }else if(firstNum > secondNum){
+            System.out.println("Второе число минимальное");
+        }else {
+            System.out.println("Числа равны");
+        }
+
+       // System.out.println(firstNum < secondNum ? "Первое число минимальное" : "Второе число минимальное" );
+    }
+}
