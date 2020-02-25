68054e95aa0b9503578dd60a8738b4c563550275
Lenovo-PC <e999or@bk.ru>
2020-02-25 22:51:51 +0300

    Lesson6
    Lesson6task3 исправлен функционал. теперь всё работает и проведена проверка

--- src/lesson6/lesson6task3/Unique.java
+++ src/lesson6/lesson6task3/Unique.java
@@ -1,34 +1,40 @@
 package lesson6.lesson6task3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Unique {
     //Написать метод, который возвращает true, если в мапе нет двух и более
     //одинаковых value, и false в противном случае. Для пустой мапы метод
     //возвращает true.
-    int count = 0;
-    List<String> valuesS;
+    public static void main(String[] args) {
+        Unique unique = new Unique();
+        Map<String, String> names = new HashMap<>();
+        names.put("1", "Smith");
+        names.put("2", "Stanley");
+        names.put("3", "Smith");
+        System.out.println(unique.isUnique(names)); // false
+        names.remove("3");
+        System.out.println(unique.isUnique(names)); // false.
+    }
+
+    
 
     public boolean isUnique(Map<String, String> map) {
         boolean rezult = true;
+        int count = 0;
+        Set<String> coincidences = new HashSet<>();
         for (String s: map.values()){
-            valuesS.add(s);
+            coincidences.add(s);
         }
-        compare(valuesS);
-        if(count > 2){
+        for (String s: map.values()) {
+            if( coincidences.contains(s)){
+                count++;
+            }
+        }
+        if (count > 2) {
             rezult = false;
         }
         return rezult;
     }
-
-    public void compare (List<String>sList){
-        for (int i = 0; i < sList.size()-1; i++) {
-            for (int j = 0; j < sList.size()-1; j++) {
-                if(sList.get(i).equals(sList.get(j))) {
-                    count++;
-                }
-            }
-        }
-    }
+    
 }
