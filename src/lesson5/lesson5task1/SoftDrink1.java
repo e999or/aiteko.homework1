aa5bb39a38218c9478938220539788a1b070a947
Lenovo-PC <e999or@bk.ru>
2020-02-28 16:35:21 +0300

    lesson 6
    Lesson6 Task2  реадизованы классы

--- src/lesson5/lesson5task1/SoftDrink1.java
+++ src/lesson5/lesson5task1/SoftDrink1.java
@@ -23,5 +23,5 @@ public enum SoftDrink1 {
     public int getCost() {
         return cost;
     }
-    
+
 }
--- src/lesson6/Lesson6task2/OneBasket.java
+++ src/lesson6/Lesson6task2/OneBasket.java
@@ -1,37 +1,47 @@
 package lesson6.Lesson6task2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OneBasket implements Basket {
-
+    Map map = new HashMap();
 
     @Override
     public void addProduct(String product, int quantity) {
-
+        map.put(product, quantity);
     }
 
     @Override
     public void removeProduct(String product) {
-
+        for (int i = 0; i < getProducts().size(); i++) {
+            if(map.containsKey(product)){
+                map.remove(product);
+            }
+        }
     }
 
     @Override
     public void updateProductQuantity(String product, int quantity) {
-
+      map.put(product, quantity);
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
 
     @Override
     public List<String> getProducts() {
-        return null;
+        List<String> list = new ArrayList<>();
+        list.addAll(map.keySet());
+        return list;
     }
 
     @Override
     public int getProductQuantity(String product) {
-        return 0;
+        int quantity = Integer.parseInt(product);
+        return quantity;
     }
 }
